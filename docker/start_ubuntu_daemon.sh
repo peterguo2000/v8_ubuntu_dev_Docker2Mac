724792d0f51533f0ee63ff70af3bd7fe2946895c
peterguo2000 <peter.k.guo@gmail.com>
2019-08-14 11:38:56 +1000

    Update start_ubuntu_daemon.sh

--- docker/start_ubuntu_daemon.sh
+++ docker/start_ubuntu_daemon.sh
@@ -1,1 +1,1 @@
-docker run -v /Users/pguo/work/codes/private_testing/node_learning/ubuntu_shared:/root/shared -it -d -p 2222:22 --name ubuntu_daemon8 ubuntu_dev /usr/sbin/sshd -D 
+docker run -v $MAC_LOCAL_LOCATION:/root/shared -it -d -p 2222:22 --name ubuntu_daemon8 ubuntu_dev /usr/sbin/sshd -D 
